313cccafc92215a102ce0b0783245fc3d9346419
John Villamil <jdsvg997@outlook.com>
2022-05-16 17:16:54 -0500

    Ordeando codigo

--- src/main/java/co/com/choucair/certification/starshap/userinterface/CreateBusnissesUnitPage.java
+++ src/main/java/co/com/choucair/certification/starshap/userinterface/CreateBusnissesUnitPage.java
@@ -8,14 +8,12 @@ public class CreateBusnissesUnitPage extends PageObject {
 
 
     public static final Target INTPUT_NAME_NEW_BUSNISSES_UNIT = Target.the("Field where we can write the name of the new Busnisses Unit")
-            .located(By.xpath("//div[@id='Serenity_Pro_Organization_BusinessUnitDialog3']//form//div[@class='field Name']//input[@id='Serenity_Pro_Organization_BusinessUnitDialog3_Name']"));
-
+            .located(By.id("Serenity_Pro_Organization_BusinessUnitDialog3_Name"));
     public static final Target TREE_PARENS_UNIT = Target.the("Tree that can show us the parents unit for the new Busnisses Unit")
-            .located(By.xpath("//div[@id='Serenity_Pro_Organization_BusinessUnitDialog3']//form//div[@class='field ParentUnitId']//span[@class='select2-arrow']"));
-
+            .located(By.className("select2-arrow"));
     public static final Target INPUT_PARENT_NEW_BUSNISSES_UNIT = Target.the("Field where we can write the parent of the new Busnisses Unit")
-            .located(By.xpath("//div[@id='select2-drop']//input"));
-
+//            .located(By.xpath("//div[@id='select2-drop']//input"));
+            .located(By.cssSelector("input.select2-input"));
     public static final Target UNIQUE_PAREN_NEW_BUSNISSES_UNIT = Target.the("Unique element taht reference us the name of the paren for the new Busnisses Unit")
             .located(By.xpath("(//div[@id='select2-drop']//ul//li)[1]"));
 
